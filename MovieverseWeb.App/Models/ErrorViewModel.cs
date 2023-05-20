9165741ba74f630105a023577218da254e147ce5
Murillo Nogueira <murillo.nogueira01@outlook.com>
2023-05-20 12:00:59 -0300

    Adicionar arquivos de projeto.

---
+++ MovieverseWeb.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieverseWeb.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MovieverseWeb.Domain/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace MovieverseWeb.Domain.Interfaces
+{
+    public interface IRepositoryBase<TEntity> where TEntity : class
+    {
+        Task<TEntity> AddAsync(TEntity entity);
+        void DeleteAsync(TEntity entity);
+        Task<IQueryable<TEntity>> GetAllAsync(params Expression<Func<TEntity, object>>[] includes);
+        Task<TEntity?> GetByIdAsync(Guid id);
+        Task<int> SaveChangesAsync();
+        Task<TEntity> UpdateAsync(TEntity entity);
+        Task<TEntity?> GetByIdAsync(Guid id, params Expression<Func<TEntity, object>>[] includes);
+    }
+}
